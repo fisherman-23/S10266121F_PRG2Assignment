110c264b5aceca2d1fa30c41de1bdf994516761e
Ranen <ranensim.yangyi@gmail.com>
2025-01-18 02:45:18 +0800

    feat: implement Terminal class

---
+++ T03_Group_PRG2Assignment/TeamRanenJingShun/Terminal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamRanenJingShun
+{
+    internal class Terminal
+    {
+        public string TerminalName { get; set; }
+        public Dictionary<string, Airline> Airlines { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; }
+
+        public Dictionary<string, double> GateFees { get; set; }
+
+        public Terminal(string name)
+        {
+            TerminalName = name;
+            Airlines = new Dictionary<string, Airline>();
+            Flights = new Dictionary<string, Flight>();
+            BoardingGates = new Dictionary<string, BoardingGate>();
+            GateFees = new Dictionary<string, double>();
+        }
+
+        public bool AddAirline(Airline airline) 
+        {
+            if (Airlines.ContainsKey(airline.Code))
+            {
+                return false;
+            }
+            else Parallel
+        }
+    }
+}
