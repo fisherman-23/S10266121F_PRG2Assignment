e20170753495c6de8ba5061ee9da2066f33ec6a7
Ranen <ranensim.yangyi@gmail.com>
2025-01-18 07:52:44 +0800

    feat: load boarding gates csv & populate BoardingGate dictionary

--- T03_Group_PRG2Assignment/TeamRanenJingShun/Program.cs
+++ T03_Group_PRG2Assignment/TeamRanenJingShun/Program.cs
@@ -16,11 +16,20 @@ using TeamRanenJingShun;
 //Console.WriteLine(al2.CalculateFees());
 
 //1 Load files (airlines and boarding gates)
-string[] airlinesCSVLines = File.ReadAllLines("assets/airlines.csv");
+string[] AirlinesCSVLines = File.ReadAllLines("assets/airlines.csv");
 Dictionary<string, Airline> AirlineDict = new Dictionary<string, Airline>();
-for (int i = 1; i < airlinesCSVLines.Length; i++)
+for (int i = 1; i < AirlinesCSVLines.Length; i++)
 {
-    string[] line = airlinesCSVLines[i].Split(',');
+    string[] line = AirlinesCSVLines[i].Split(',');
     Airline airline = new Airline(line[0], line[1]);
     AirlineDict[line[1]] = airline;
+}
+
+string[] BoardingGatesCSVLines = File.ReadAllLines("assets/boardinggates.csv");
+Dictionary<string, BoardingGate> BoardingGateDict = new Dictionary<string, BoardingGate>();
+for (int i = 1; i < BoardingGatesCSVLines.Length; i++)
+{
+    string[] line = BoardingGatesCSVLines[i].Split(',');
+    BoardingGate boardingGate = new BoardingGate(line[0], Convert.ToBoolean(line[2]), Convert.ToBoolean(line[1]), Convert.ToBoolean(line[3]));
+    BoardingGateDict[line[0]] = boardingGate;
 }
