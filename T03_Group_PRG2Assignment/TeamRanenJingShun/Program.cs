76ea38f7c3f11d99420e6be7332abb7c6059a628
Ranen <ranensim.yangyi@gmail.com>
2025-01-18 06:57:49 +0800

    chore: comment out test code to prepare for branching

--- T03_Group_PRG2Assignment/TeamRanenJingShun/Program.cs
+++ T03_Group_PRG2Assignment/TeamRanenJingShun/Program.cs
@@ -2,15 +2,16 @@
 using TeamRanenJingShun;
 
 // Temporary code to test the classes
-Flight fl = new NORMFlight("SQ123", "DXB", "SIN", new DateTime(2025,1,1), "On Time");
-Flight fl2 = new CFFTFlight("SQ122", "DXB", "SIN", new DateTime(2025, 1, 1), "On Time");
+//Flight fl = new NORMFlight("SQ123", "DXB", "SIN", new DateTime(2025,1,1), "On Time");
+//Flight fl2 = new CFFTFlight("SQ122", "DXB", "SIN", new DateTime(2025, 1, 1), "On Time");
 
-Console.WriteLine(fl.CalculateFees());
-Console.WriteLine(fl2.CalculateFees());
-Airline al = new Airline("SQ","123");
-Airline al2 = new Airline("SQ", "122");
+//Console.WriteLine(fl.CalculateFees());
+//Console.WriteLine(fl2.CalculateFees());
+//Airline al = new Airline("SQ","123");
+//Airline al2 = new Airline("SQ", "122");
 
-al.AddFlight(fl);
-al2.AddFlight(fl2);
-Console.WriteLine(al.CalculateFees());
-Console.WriteLine(al2.CalculateFees());
+//al.AddFlight(fl);
+//al2.AddFlight(fl2);
+//Console.WriteLine(al.CalculateFees());
+//Console.WriteLine(al2.CalculateFees());
+
