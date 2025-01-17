ff2c2d99668cd734047051674c0ead771ade7935
Ranen <ranensim.yangyi@gmail.com>
2025-01-18 06:51:09 +0800

    feat: implement DDJBFlight & LWTTFlight classes

---
+++ T03_Group_PRG2Assignment/TeamRanenJingShun/LWTTFlight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamRanenJingShun
+{
+    internal class LWTTFlight : Flight
+    {
+        public double RequestFee { get; set; }
+        public LWTTFlight(string flightNo, string origin, string dest, DateTime et, string status) : base(flightNo, origin, dest, et, status)
+        {
+            RequestFee = 500;
+        }
+        public override double CalculateFees()
+        {
+            return base.CalculateFees() + RequestFee;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "RequestFee: " + RequestFee;
+        }
+    }
+}
