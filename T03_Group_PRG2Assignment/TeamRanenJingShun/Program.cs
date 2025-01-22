682b6c2b263cf0190ea9d14b076123016f8fd6af
Ranen <ranensim.yangyi@gmail.com>
2025-01-23 02:09:00 +0800

    feat: implement flight selection and display details

--- T03_Group_PRG2Assignment/TeamRanenJingShun/Program.cs
+++ T03_Group_PRG2Assignment/TeamRanenJingShun/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
 using TeamRanenJingShun;
 
 // Temporary code to test the classes
@@ -180,45 +181,141 @@ void DisplayFlightFromAirline(Terminal terminal)
         {
             code = Console.ReadLine().ToUpper();
         }
-        catch 
+        catch
         {
             Console.WriteLine("Invalid input, please try again.");
             continue;
         }
-        break;
-    }
+        
 
-    Airline? airline = null;
-    foreach (KeyValuePair<string, Airline> kvp in terminal.Airlines)
-    {
-        if (kvp.Key == code)
+        Airline? airline = null;
+        foreach (KeyValuePair<string, Airline> kvp in terminal.Airlines)
         {
-            airline = kvp.Value;
+            if (kvp.Key == code)
+            {
+                airline = kvp.Value;
+            }
         }
-    }
-    if (airline != null)
-    {  
-        if (airline.Flights.Count == 0)
-        {
-            Console.WriteLine($"No flights available for airline {airline.Name}");
+        if (airline != null)
+        {  
+            if (airline.Flights.Count == 0)
+            {
+                Console.WriteLine($"No flights available for {airline.Name}");
+
+            }
+            else
+            {
+                Console.WriteLine($"{"Airline Number", -10} {"Origin", -20} {"Destination"}");
+                Console.WriteLine("Departure/Arrival Time");
+                foreach (KeyValuePair<string, Flight> kvp in airline.Flights)
+                {
+                    Flight flight = kvp.Value;
+                    Console.WriteLine($"{flight.FlightNumber, -10} {flight.Origin, -25} {flight.Destination, -20}");
+                }
+                DisplayFlightDetails(airline);
+            }
+            break;
         }
         else
         {
-            Console.WriteLine($"{"Flight Number", -20} {"Airline Name", -25} {"Origin", -20} {"Destination", -20} Expected");
-            Console.WriteLine("Departure/Arrival Time");
-            foreach (KeyValuePair<string, Flight> kvp in airline.Flights)
+            Console.WriteLine("Airline not found");
+            continue;
+        }
+    }
+
+}
+
+DisplayFlightFromAirline(Terminal5);
+
+void DisplayFlightDetails(Airline airline)
+{
+    string? code = null;
+    while (true)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Enter flight number: ");
+        try
+        {
+            code = Console.ReadLine().ToUpper();
+        }
+        catch
+        {
+            Console.WriteLine("Invalid input, please try again.");
+            continue;
+        }
+
+        foreach(KeyValuePair<String, Flight> kvp in airline.Flights)
+        {
+            Flight flight = kvp.Value;
+            if (kvp.Key == code)
             {
-                Flight flight = kvp.Value;
+                Console.WriteLine($"Full flight details for {flight.FlightNumber}");
                 string date = flight.ExpectedTime.ToString("dd/MM/yyyy");
                 string time = flight.ExpectedTime.ToString("hh:mm tt") + ":00";
-                Console.WriteLine($"{flight.FlightNumber, -20} {airline.Name, -25} {flight.Origin, -20} {flight.Destination, -20} {date}");
+                Console.WriteLine($"{"Flight Number", -10} {"Airline Name", -25} {"Origin", -15} {"Destination", -15} {"Expected", -10}");
+
+                bool sr = false;
+                bool bg = false;
+                BoardingGate? boardingGate = null;
+                if (flight.GetType() == typeof(CFFTFlight) || flight.GetType() == typeof(DDJBFlight) || flight.GetType() == typeof(LWTTFlight))
+                {
+                    Console.Write($"{"Special Request Code",-20}");
+                    sr = true;
+                }
+                if (!(flight.GetType() == typeof(NORMFlight))) {
+                    Console.Write($"{"Boarding Gate", -15}");
+                    bg = true;
+                }
+                Console.WriteLine();
+                Console.WriteLine($"{flight.FlightNumber, -10} {airline.Name, -25} {flight.Origin, -15} {flight.Destination, -15} {date}");
                 Console.WriteLine(time);
+                if (sr)
+                {
+                    Console.Write($"{GetRequestCode(flight),-20}");
+                }
+                if (bg)
+                {
+                    Console.Write($"{GetBoardingGate(flight)}");
+                }
+                return;
             }
         }
     }
+}
+
+string[]? GetBoardingGate(Flight flight)
+{
+    if (flight is CFFTFlight)
+    {
+        return new string[] { "B1", "B2", "B3", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20", "C21", "C22" };
+    }
+    else if (flight is DDJBFlight)
+    {
+        return new string[] { "A10", "A11", "A12", "A13", "A20", "A21", "A22", "B10", "B11", "B12" };
+    }
+    else if (flight is LWTTFlight)
+    {
+        return new string[] { "A1", "A2", "A20", "A21", "A22", "C14", "C15", "C16", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "B13", "B14", "B15", "B16", "B17", "B18", "B19", "B20", "B21", "B22" };
+    }
+    return null;
+}
+
+string GetRequestCode(Flight flight)
+{
+    if (flight is DDJBFlight)
+    {
+        return "DDJB";
+    }
+    else if (flight is CFFTFlight)
+    {
+        return "CFFT";
+    }
+    else if (flight is LWTTFlight)
+    {
+        return "LWTT";
+    }
     else
     {
-        Console.WriteLine("Airline not found");
+        return "NORM";
     }
 }
-DisplayFlightFromAirline(Terminal5);
